e9a9cf1925cd48e6a5065a2ab9b8969c4f314959
foggy21 <rashit.xaliullin@inbox.ru>
2022-05-31 14:38:54 +1000

    Added navigators on news page

--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request): #Функция принимает экзампляр класса httpRequest. Там хранятся все данные о пользователе, куки-шмуки и т.д
+    #print(dir(request))
+    return HttpResponse('Hello World!')
+
+def test(reques):
+    return HttpResponse('<h1>Tested Page :)</h1>')
