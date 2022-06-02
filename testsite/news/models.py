78bd57895b5ec814ccf654917ee35935f4b6cb0d
foggy21 <rashit.xaliullin@inbox.ru>
2022-06-02 23:37:35 +1000

    Create model for news

--- testsite/news/models.py
+++ testsite/news/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.forms import BooleanField
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    is_published = models.BooleanField(default=False)
+
