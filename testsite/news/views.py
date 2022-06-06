3fdc7d3e5bb2ad553f274406f7d74ff9b0d563f3
foggy21 <rashit.xaliullin@inbox.ru>
2022-06-06 23:14:23 +1000

    Remake function index under render

--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request): #Функция принимает экзампляр класса httpRequest. Там хранятся все данные о пользователе, куки-шмуки и т.д
-    #print(dir(request))
-    return HttpResponse('Hello World!')
-
-def test(reques):
-    return HttpResponse('<h1>Tested Page :)</h1>')
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news':news, 'title': 'Список новостей'})
