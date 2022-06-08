a3b974ee86ba32995d1bfc9a79289b76d0f4b5f7
foggy21 <rashit.xaliullin@inbox.ru>
2022-06-08 18:10:34 +1000

    News connected to Category in db

--- testsite/news/models.py
+++ testsite/news/models.py
@@ -1,7 +1,15 @@
-from distutils.command.upload import upload
-from tabnanny import verbose
 from django.db import models
-from django.forms import BooleanField
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, db_index=True, verbose_name="Категория")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta():
+        verbose_name = "Категория"
+        verbose_name_plural = "категории"
+        ordering = ['title']
 
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name="Статья")
@@ -10,6 +18,7 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото", blank=True)
     is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name="Категория")
 
     def __str__(self):
         return self.title
